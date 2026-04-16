94c71fbf09dc6f5cdadb6c6a342f4afb98ff60b7
fobihane-debug <fobihane@gmail.com>
2026-04-16 20:59:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template_string, request
 import feedparser
 from telegram import Bot
 from apscheduler.schedulers.background import BackgroundScheduler
-import snscrape.modules.twitter as sntwitter
 
 TOKEN = os.getenv("8625470192:AAGCLRkQzxgXJCrOdz9RQPXz6TMvlQz716I")
 CHANNEL = "@kuponbazz"
@@ -11,26 +10,18 @@ CHANNEL = "@kuponbazz"
 bot = Bot(token=TOKEN)
 app = Flask(__name__)
 
-# Haber siteleri
+# SADECE HABER SİTELERİ
 feeds = [
     "https://www.haberturk.com/rss/spor.xml",
     "https://www.fotomac.com.tr/rss/anasayfa.xml"
 ]
 
-# X hesapları
-accounts = [
-    "FabrizioRomano",
-    "TransferNewsTR"
-]
-
 news_cache = []
-sent = set()
 
 def fetch_news():
     global news_cache
     news_cache = []
 
-    # RSS haberleri
     for url in feeds:
         feed = feedparser.parse(url)
         for entry in feed.entries[:5]:
@@ -38,22 +29,6 @@ def fetch_news():
                 "title": entry.title,
                 "link": entry.link
             })
-
-    # X (Twitter) haberleri
-    for user in accounts:
-        try:
-            for tweet in sntwitter.TwitterUserScraper(user).get_items():
-                if len(news_cache) > 20:
-                    break
-
-                if tweet.content not in sent:
-                    news_cache.append({
-                        "title": tweet.content,
-                        "link": tweet.url
-                    })
-                    sent.add(tweet.content)
-        except:
-            pass
 
 def send_news(title, link):
     msg = f"🔥 *{title}*\n\nDetay 👇\n{link}"
@@ -78,11 +53,13 @@ def home():
     """
     return render_template_string(html, news=news_cache)
 
+# HER 30 DAKİKADA HABER ÇEK
 scheduler = BackgroundScheduler()
 scheduler.add_job(fetch_news, 'interval', minutes=30)
 scheduler.start()
 
 fetch_news()
 
+# RAILWAY PORT
 PORT = int(os.getenv("PORT", 5000))
 app.run(host="0.0.0.0", port=PORT)
